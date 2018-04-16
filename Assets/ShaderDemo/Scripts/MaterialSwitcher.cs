e2dc4d3807792b1777b3d45f9bf931f7ac5be69f
Owen Hall <owen.hall@red-gate.com>
2018-04-16 21:55:01 +0100

    A few scenes with different vertex shaders being demonstrated.
    SceneSwitcher class used to swap between scenes with keyboard num bar.
    
    Knight asset from store: https://www.assetstore.unity3d.com/en/?stay#!/content/83586

---
+++ Assets/ShaderDemo/Scripts/MaterialSwitcher.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+namespace Assets.ShaderDemo.Scripts
+{
+    [RequireComponent(typeof(Renderer))]
+    public class MaterialSwitcher : MonoBehaviour
+    {
+        public Material[] Materials;
+
+        private Renderer _meshRenderer;
+        private int _currentIndex;
+
+        public void Start()
+        {
+            if (Materials == null || Materials.Length == 0)
+            {
+                throw new ArgumentException("Must have at least one material!");
+            }
+
+            _meshRenderer = GetComponent<Renderer>();
+        }
+
+        public void Update()
+        {
+            if (Input.anyKeyDown)
+            {
+                Switch();
+            }
+        }
+
+        private void Switch()
+        {
+            _currentIndex = (_currentIndex + 1) % Materials.Length;
+            _meshRenderer.materials = Enumerable.Repeat(Materials[_currentIndex], _meshRenderer.materials.Length).ToArray();
+        }
+    }
+}
---
+++ Assets/ShaderDemo/Scripts/SceneSwitcher.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.ShaderDemo.Scripts
+{
+    public class SceneSwitcher : MonoBehaviour
+    {
+        private readonly KeyCode[] _numKeyCodes = {
+            KeyCode.Alpha0,
+            KeyCode.Alpha1,
+            KeyCode.Alpha2,
+            KeyCode.Alpha3,
+            KeyCode.Alpha4,
+            KeyCode.Alpha5,
+            KeyCode.Alpha6,
+            KeyCode.Alpha7,
+            KeyCode.Alpha8,
+            KeyCode.Alpha9,
+        };
+
+        private const int KeyCodeZeroToActualZeroOffset = -48;
+
+        public void Update()
+        {
+            if (Input.anyKeyDown)
+            {
+                var numKeyDown = _numKeyCodes.FirstOrDefault(Input.GetKeyDown);
+                if (numKeyDown != 0)
+                {
+                    var sceneIndex = (int)numKeyDown + KeyCodeZeroToActualZeroOffset; // horrid enum cast
+                    SceneManager.LoadScene(sceneIndex);
+                }
+            }
+        }
+    }
+}
